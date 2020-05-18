864921968c16dfa39c4aca190041b4534cd37232
Brock Pond <heidipond23@gmail.com>
2020-05-18 10:13:21 -0600

    fixed date and folder

--- lesson4/js/script.js
+++ lesson4/js/script.js
@@ -1,3 +1,12 @@
 const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
+const mainnav   = document.querySelector('.navigation')
 hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+var dateObj = new Date();
+var weekday = dateObj.toLocaleString('default', { weekday: 'long' });
+var day     = dateObj.getUTCDate();
+var month   = dateObj.toLocaleString('default', { month: 'long' });
+var year    = dateObj.getUTCFullYear();
+
+var newdate = weekday + ", " + day + " " + month  + " " + year;
+document.getElementById("newdate").innerHTML = newdate;
