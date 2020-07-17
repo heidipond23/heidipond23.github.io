0d883d79786466f71e7deebbe0738dcde04d7a62
heidipond23 <heidipond23@gmail.com>
2020-07-16 22:19:15 -0600

    created rentals table

--- scoots/js/forecast.js
+++ scoots/js/forecast.js
@@ -1,13 +1,15 @@
-const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=17937c6d2819c6a178252b10a66c0a19";
+const weatherURL = "https://api.openweathermap.org/data/2.5/weather?id=3530103&units=imperial&APPID=17937c6d2819c6a178252b10a66c0a19";
 fetch(weatherURL)
   .then((response) => response.json())
   .then((jsObject) => {
- 
+    console.log(jsObject);
+
     document.getElementById('currentTemp').textContent = Math.round(jsObject.main.temp, 0);
     document.getElementById('condition').textContent = jsObject.weather[0].description;
     document.getElementById('humidity').textContent = jsObject.main.humidity;
-    
+
   });
+
 
   const forecastURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=17937c6d2819c6a178252b10a66c0a19";
 fetch(forecastURL)
@@ -15,59 +17,10 @@ fetch(forecastURL)
     .then((jsObject) => {
         console.log(jsObject);
 
-
-
-        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('12:00:00'));
+        const fivedayforecast = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
         console.log(fivedayforecast);
-
-       let fifthDay = fivedayforecast[4];
-        let d = new Date(fifthDay.dt_txt);
-        let div = document.createElement('div');
-        let pDay = document.createElement('p');
-        let pForecast = document.createElement('p');
-        let spanDay = document.createElement('span');
-        spanDay.setAttribute('id', `dayofweek${day+1}`);
-
-        div.appendChild(pDay);
-        pDay.appendChild(spanDay);
-        pForecast.appendChild(spanF);
-
-        document.querySelector('div.forecastdiv').appendChild(div);
-        document.getElementById(`forecast${day+1}`).textContent = Math.round(fifthDay.main.temp, 0);
-        document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
 
 
     
 
-
-       /* const weekdays = ['Sun',  'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        let day = 0;
-        fivedayforecast.forEach(forecast => {
-            let d = new Date(forecast.dt_txt);
-            const imagesrc = 'https://openweathermap.org/img/w/' + forecast.weather[0].icon + '.png'; // note the concatenation
-            const desc = forecast.weather[0].description; // note how we reference the weather array
-
-            let div = document.createElement('div');
-            let pDay = document.createElement('p');
-            let spanDay = document.createElement('span');
-            spanDay.setAttribute('id', `dayofweek${day+1}`);
-            let icon = document.createElement('img');
-            icon.setAttribute('src', imagesrc);
-            icon.setAttribute('alt', desc);
-            let pForecast = document.createElement('p');
-            let spanForecast = document.createElement('span');
-            spanForecast.setAttribute('id', `forecast${day+1}`);
-
-            div.appendChild(pDay);
-            pDay.appendChild(spanDay);
-            div.appendChild(icon);
-            div.appendChild(pForecast);
-            pForecast.appendChild(spanForecast);
-
-            document.querySelector('div.forecastdiv').appendChild(div);
-            document.getElementById(`forecast${day+1}`).textContent = Math.round(forecast.main.temp, 0);
-            document.getElementById(`dayofweek${day+1}`).textContent = weekdays[d.getDay()];
-
-            day++;
-        });*/
     });
--- scoots/js/rentals.js
+++ scoots/js/rentals.js
@@ -1,31 +1,87 @@
-const requestURL = '';
+const requestURL = 'https://heidipond23.github.io/scoots/data/rent.json';
+console.log(requestURL);
+
+
 
 fetch(requestURL)
     .then(function (response) {
         return response.json();
+
     })
     .then(function (jsonObject) {
         //console.table(jsonObject); // temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let bday = document.createElement('p');
-            let place = document.createElement('p');
-            let image = document.createElement("img");
+        const rentals = jsonObject['rentals'];
+        let table = document.createElement('table');
+        let thead = document.createElement('thead');
+        let caption = document.createElement('caption');
+        let th = document.createElement('th');
+        let tr = document.createElement('tr');
+        let tr2 = document.createElement('tr');
+        let reservation = document.createElement('th');
+        let walkin = document.createElement('th');
+        let rentaltype = document.createElement('th');
+        let maxpersons = document.createElement('th');
+        let halfday = document.createElement('th');
+        let fullday = document.createElement('th');
+        let halfday2 = document.createElement('th');
+        let fullday2 = document.createElement('th');
 
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            bday.textContent = 'Date of Birth: ' + prophets[i].birthdate;
-            place.textContent = 'Place of Birth: ' +  prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + ' - ' + prophets[i].order);
+        caption.textContent = "Max Persons and Price Chart (Includes Tax)";
+        reservation.textContent = "Reservation";
+        walkin.textContent = "Walk-In";
+        rentaltype.textContent = "Rental Type";
+        maxpersons.textContent = "Max. Persons";
+        halfday.textContent = "Half Day (3hrs)";
+        fullday.textContent = "Full Day";
+        halfday2.textContent = "Half Day (3hrs)";
+        fullday2.textContent = "Full Day";
 
 
-            card.appendChild(h2);
-            card.appendChild(bday);
-            card.appendChild(place);
-            card.appendChild(image);
+        table.appendChild(thead);
+        thead.appendChild(caption);
+        thead.appendChild(tr);
+        tr.appendChild(th);
+        tr.appendChild(reservation);
+        tr.appendChild(walkin);
+        thead.appendChild(tr2);
+        tr2.appendChild(rentaltype);
+        tr2.appendChild(maxpersons);
+        tr2.appendChild(halfday);
+        tr2.appendChild(fullday);
+        tr2.appendChild(halfday2);
+        tr2.appendChild(fullday2);
 
-            document.querySelector('div.cards').appendChild(card);
+
+        for (let i = 0; i < rentals.length; i++) {
+
+            let tr = document.createElement('tr');
+            let vehicle = document.createElement('td');
+            let persons = document.createElement('td');
+            let reserveHalf = document.createElement('td');
+            let reserveFull = document.createElement('td');
+            let walkHalf = document.createElement('td');
+            let walkFull = document.createElement('td');
+
+
+
+            vehicle.textContent = rentals[i].vehicle;
+            persons.textContent = rentals[i].persons;
+            reserveHalf.textContent = rentals[i].reserveHalf;
+            reserveFull.textContent = rentals[i].reserveFull;
+            walkHalf.textContent = rentals[i].walkHalf;
+            walkFull.textContent = rentals[i].walkFull;
+
+
+
+            table.appendChild(vehicle);
+            table.appendChild(persons);
+            table.appendChild(reserveHalf);
+            table.appendChild(reserveFull);
+            table.appendChild(walkHalf);
+            table.appendChild(walkFull);
+            table.appendChild(tr);
+
         }
+
+        document.querySelector('div.table').appendChild(table);
     });
